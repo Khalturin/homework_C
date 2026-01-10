b158c5468dd24a6b4baac927c626b61d6761acd1
mrChiffa <mrjokko@gmail.com>
2026-01-10 16:09:23 +0300

    API для получение температур. Makefile.

---
+++ SensorProject/temp_api.c
@@ -0,0 +1,22 @@
+#include "temp_api.h"
+
+float mid_month_temp(sensor_t *info){
+
+    return 0;
+}
+float min_month_temp(sensor_t *info){
+    return 0;
+}
+float max_month_temp(sensor_t *info){
+    return 0;
+}
+
+float mid_year_temp(sensor_t *info){
+        return 0;
+}
+float min_year_temp(sensor_t *info){
+    return 0;
+}
+float max_year_temp(sensor_t *info){
+    return 0;
+}
